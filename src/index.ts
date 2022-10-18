86a350af9aa63f611f8d951c6d06afd6ede29813
Fahad almani <Fahadalmana780@gmail.com>
2022-10-18 20:17:04 +0300

    fix spelling error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import routers from "./routes/routes_controler";
+import routers from "./routes/routes_controller";
 import { join } from "path";
 
 const app = express();
